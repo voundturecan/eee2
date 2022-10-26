17bff70b8f14d3c94f0263a0fe64653b299866ff
voundturecan <107836074+voundturecan@users.noreply.github.com>
2022-10-26 13:25:58 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-cb0d2a83c8404b51b7616cb0b0361e47}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
